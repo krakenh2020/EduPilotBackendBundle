7b3480c098f2e060a3067f076d178db5244eca1b
PeterTheOne <petertheone@gmail.com>
2021-02-11 12:26:03 +0100

    remove is_granted attribute on credential

--- src/Entity/Credential.php
+++ src/Entity/Credential.php
@@ -11,7 +11,6 @@ use Symfony\Component\Serializer\Annotation\Groups;
 // todo: schema
 /**
  * @ApiResource(
- *     attributes={"security"="is_granted()"},
  *     collectionOperations={"get"},
  *     itemOperations={"get", "put", "delete"},
  *     iri="https://schema.org/Place",
