7d896dd4d2143f09b780f7c9fd4a57db5139abcc
Stefan More <stefan.more@iaik.tugraz.at>
2021-10-19 16:21:45 +0200

    added simple test for credential offers

--- tests/Service/DidExternalApiTest.php
+++ tests/Service/DidExternalApiTest.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use PHPUnit\Framework\TestCase;
 use VC4SM\Bundle\Service\DidExternalApi;
+use VC4SM\Bundle\Service\ExternalApi;
 
 class DidExternalApiTest extends TestCase
 {
@@ -18,11 +19,14 @@ class DidExternalApiTest extends TestCase
         //self::bootKernel();
 
         //$this->api = new DidExternalApi();
+
+        DidExternalApi::$classLogger = new MockLogger();
     }
 
-    public function testEcho()
+    public function testLogger()
     {
-        echo "test echo!";
+        DidExternalApi::$classLogger->info("Init logger");
+
         $this->assertTrue(true);
     }
 
@@ -33,5 +37,69 @@ class DidExternalApiTest extends TestCase
         $res = DidExternalApi::requestInsecure($url, "GET");
         //print_r($res);
         $this->assertEquals(200, $res['status_code']);
+    }
+
+    public function testAgentConnection()
+    {
+
+        $ret1 = DidExternalApi::checkConnection("https://kraken.iaik.tugraz.at");
+        $this->assertTrue($ret1);
+
+        $ret2 = DidExternalApi::checkConnection("https://krakenh2020.eu");
+        $this->assertFalse($ret2);
+    }
+
+    public function testBuildOfferRequestDiploma()
+    {
+        $mydid = "did:myDID";
+        $theirdid = "did:theirDID";
+        $api = new ExternalApi();
+
+        $type = "diplomas";
+        $cred_id = "bsc1";
+        $offer = DidExternalApi::buildOfferRequest($mydid, $theirdid, $api, $type, $cred_id);
+        //print_r($offer);
+        $cred_json = json_encode($offer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        DidExternalApi::$classLogger->info($cred_json);
+
+        $this->assertEquals($mydid, $offer['my_did']);
+        $this->assertEquals($theirdid, $offer['their_did']);
+    }
+
+    public function testBuildOfferRequestGrade()
+    {
+        $mydid = "did:myDID";
+        $theirdid = "did:theirDID";
+        $api = new ExternalApi();
+
+        $type = "course-grades";
+        $cred_id = "os";
+        $offer = DidExternalApi::buildOfferRequest($mydid, $theirdid, $api, $type, $cred_id);
+        //print_r($offer);
+        $cred_json = json_encode($offer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        DidExternalApi::$classLogger->info($cred_json);
+
+        $this->assertEquals($mydid, $offer['my_did']);
+        $this->assertEquals($theirdid, $offer['their_did']);
+    }
+
+
+}
+
+
+
+
+
+class MockLogger
+{
+
+    public function warning($text)
+    {
+        echo "Warning: " . $text . "\n";
+    }
+
+    public function info($text)
+    {
+        echo "Info: " . $text . "\n";
     }
 }
