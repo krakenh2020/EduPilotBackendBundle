1d3371cb06483789887a0b82d7c0ca1d76e69487
Stefan More <stefan.more@iaik.tugraz.at>
2022-03-31 15:03:08 +0200

    added separate DIDs for uni and student

--- .github/helper/kms_init.py
+++ .github/helper/kms_init.py
@@ -2,6 +2,11 @@ import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+// keys generated using https://github.com/transmute-industries/did-key.js
+// via https://www.npmjs.com/package/@transmute/did-key-ed25519
+// using https://gist.github.com/stefan2904/f1ac36cebe946a475bcbbb8c3e8960c9
+
+
 agent_url = 'http://localhost:8082'
 
 print('Check connection uni ...')
@@ -21,6 +26,8 @@ didkey = {
  "d": "Rh4-MEIiWoC8clgA2mnT_CAjiSQ0OeO96BYI0mrcmYE"
  }
 
+print('Importing {}'.format(did))
+
 r = requests.post(agent_url + '/kms/import', json=didkey, verify=False)
 print('KMS Import: {} {}'.format(r.status_code, r.text))
 
@@ -37,15 +44,17 @@ if r.status_code != 200:
     exit()
 print('Connection to student agent works!')
 
-did = 'did:key:z6MkwZ9XcVLTNwkv8ELoxPu5q2dMkqLnE422ex69YMVX4hpr'
+did = 'did:key:z6Mkk7yqnGF3YwTrLpqrW6PGsKci7dNqh1CjnvMbzrMerSeL'
 didkey = {
  "kty":"OKP",
- "kid":"z6MkwZ9XcVLTNwkv8ELoxPu5q2dMkqLnE422ex69YMVX4hpr",
+ "kid":"z6Mkk7yqnGF3YwTrLpqrW6PGsKci7dNqh1CjnvMbzrMerSeL",
  "crv":"Ed25519",
  "alg":"EdDSA",
- "x": "_hjLQG4OZMUagNFaKNvOkPTTzpKVWC2eKCdgN4QILM8",
- "d": "Rh4-MEIiWoC8clgA2mnT_CAjiSQ0OeO96BYI0mrcmYE"
+ "x": "VDXDwuGKVq91zxU6q7__jLDUq8_C5cuxECgd-1feFTE",
+ "d": "T2azVap7CYD_kB8ilbnFYqwwYb5N-GcD6yjGEvquZXg"
  }
+
+print('Importing {}'.format(did))
 
 r = requests.post(agent_url + '/kms/import', json=didkey, verify=False)
 print('KMS Import: {} {}'.format(r.status_code, r.text))
