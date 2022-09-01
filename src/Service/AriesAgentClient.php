07a283b5d4341b84659c9ddd61a4df199f77fee2
Stefan More <stefan.more@iaik.tugraz.at>
2022-09-01 15:47:12 +0200

    formatting

--- src/Service/AriesAgentClient.php
+++ src/Service/AriesAgentClient.php
@@ -26,7 +26,7 @@ class AriesAgentClient
     public function checkConnection(): bool
     {
         $PATH_CONNECTIONS = '/connections';
-        $url = $this->agentUrl.$PATH_CONNECTIONS;
+        $url = $this->agentUrl . $PATH_CONNECTIONS;
 
         try {
             $res = SimpleHttpClient::request($url);
@@ -35,7 +35,7 @@ class AriesAgentClient
         }
 
         if ($res['status_code'] !== 200) {
-            $this->logger->warning("Checked connection to $url, status code: ".$res['status_code']);
+            $this->logger->warning("Checked connection to $url, status code: " . $res['status_code']);
 
             return false;
         }
@@ -45,7 +45,7 @@ class AriesAgentClient
 
     private function printError($methodname, $res)
     {
-        $this->logger->warning($methodname.' status code: '.$res['status_code'].' -> '.$res['contents']);
+        $this->logger->warning($methodname . ' status code: ' . $res['status_code'] . ' -> ' . $res['contents']);
     }
 
     private function request(string $methodname, string $method, string $url, array $data = [])
@@ -68,7 +68,7 @@ class AriesAgentClient
     public function createInvitation(string $alias = 'TU Graz KRAKEN Demo'): ?string
     {
         $PATH_CREATE_INVITATION = '/connections/create-invitation';
-        $url = $this->agentUrl.$PATH_CREATE_INVITATION.'?alias='.urlencode($alias);
+        $url = $this->agentUrl . $PATH_CREATE_INVITATION . '?alias=' . urlencode($alias);
 
         return $this->request('createInvitation', 'POST', $url);
     }
@@ -76,15 +76,15 @@ class AriesAgentClient
     public function receiveConnectionInvite($invite): ?string
     {
         $PATH_RECEIVE_INVITATION = '/connections/receive-invitation';
-        $url = $this->agentUrl.$PATH_RECEIVE_INVITATION;
+        $url = $this->agentUrl . $PATH_RECEIVE_INVITATION;
 
         return $this->request('receiveConnectionInvite', 'POST', $url, $invite);
     }
 
     public function acceptConnectionInvite(string $connectionId): ?string
     {
-        $PATH_ACCEPT_INVITATION = '/connections/'.$connectionId.'/accept-invitation';
-        $url = $this->agentUrl.$PATH_ACCEPT_INVITATION;
+        $PATH_ACCEPT_INVITATION = '/connections/' . $connectionId . '/accept-invitation';
+        $url = $this->agentUrl . $PATH_ACCEPT_INVITATION;
 
         return $this->request('acceptConnectionInvite', 'POST', $url);
     }
@@ -92,23 +92,23 @@ class AriesAgentClient
     public function listConnections(): ?string
     {
         $PATH_GET_CONNECTIONS = '/connections';
-        $url = $this->agentUrl.$PATH_GET_CONNECTIONS;
+        $url = $this->agentUrl . $PATH_GET_CONNECTIONS;
 
         return $this->request('listConnections', 'GET', $url);
     }
 
     public function getConnectionById(string $id): ?string
     {
-        $PATH_GET_CONNECTION = '/connections/'.$id;
-        $url = $this->agentUrl.$PATH_GET_CONNECTION;
+        $PATH_GET_CONNECTION = '/connections/' . $id;
+        $url = $this->agentUrl . $PATH_GET_CONNECTION;
 
         return $this->request('getConnectionById', 'GET', $url);
     }
 
     public function acceptInviteRequest(string $identifier): ?string
     {
-        $PATH_ACCEPT_INVITATION = '/connections/'.$identifier.'/accept-request';
-        $url = $this->agentUrl.$PATH_ACCEPT_INVITATION;
+        $PATH_ACCEPT_INVITATION = '/connections/' . $identifier . '/accept-request';
+        $url = $this->agentUrl . $PATH_ACCEPT_INVITATION;
 
         return $this->request('acceptInviteRequest', 'POST', $url);
     }
@@ -116,15 +116,15 @@ class AriesAgentClient
     public function sendOfferRequest($credoffer): ?string
     {
         $PATH_SEND_OFFER = '/issuecredential/send-offer';
-        $url = $this->agentUrl.$PATH_SEND_OFFER;
+        $url = $this->agentUrl . $PATH_SEND_OFFER;
 
         return $this->request('sendOfferRequest', 'POST', $url, $credoffer);
     }
 
     public function acceptRequestRequest(string $credoffer_piid, array $cred): ?string
     {
-        $PATH_ACCEPT_RREQUEST = '/issuecredential/'.$credoffer_piid.'/accept-request';
-        $url = $this->agentUrl.$PATH_ACCEPT_RREQUEST;
+        $PATH_ACCEPT_RREQUEST = '/issuecredential/' . $credoffer_piid . '/accept-request';
+        $url = $this->agentUrl . $PATH_ACCEPT_RREQUEST;
 
         return $this->request('acceptRequestRequest', 'POST', $url, $cred);
     }
@@ -132,7 +132,7 @@ class AriesAgentClient
     public function getIssuercredentialActions(): ?array
     {
         $PATH_GET_CREDACTIONS = '/issuecredential/actions';
-        $url = $this->agentUrl.$PATH_GET_CREDACTIONS;
+        $url = $this->agentUrl . $PATH_GET_CREDACTIONS;
 
         $r = $this->request('getIssuercredentialActions', 'GET', $url);
 
@@ -141,8 +141,8 @@ class AriesAgentClient
 
     public function acceptCredentialOffer(string $credoffer_piid): ?string
     {
-        $PATH_ACCEPT_CREDOFFER = '/issuecredential/'.$credoffer_piid.'/accept-offer';
-        $url = $this->agentUrl.$PATH_ACCEPT_CREDOFFER;
+        $PATH_ACCEPT_CREDOFFER = '/issuecredential/' . $credoffer_piid . '/accept-offer';
+        $url = $this->agentUrl . $PATH_ACCEPT_CREDOFFER;
 
         return $this->request('acceptCredentialOffer', 'POST', $url);
     }
@@ -150,7 +150,7 @@ class AriesAgentClient
     public function signCredential(array $cred): ?string
     {
         $PATH_SIGN_CRED = '/verifiable/signcredential';
-        $url = $this->agentUrl.$PATH_SIGN_CRED;
+        $url = $this->agentUrl . $PATH_SIGN_CRED;
 
         $credJson = json_encode($cred);
         $this->logger->info("Signing a credential using $url: $credJson");
@@ -160,8 +160,8 @@ class AriesAgentClient
 
     public function acceptCredential(string $credoffer_piid, string $cred_name): ?string
     {
-        $PATH_ACCEPT_CRED = '/issuecredential/'.$credoffer_piid.'/accept-credential';
-        $url = $this->agentUrl.$PATH_ACCEPT_CRED;
+        $PATH_ACCEPT_CRED = '/issuecredential/' . $credoffer_piid . '/accept-credential';
+        $url = $this->agentUrl . $PATH_ACCEPT_CRED;
 
         return $this->request('acceptCredential', 'POST', $url, ['names' => [$cred_name]]);
     }
